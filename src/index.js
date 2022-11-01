fcba244f3ba7a68f70d7f678061b45775742de54
ramondti <ramon@dataintegra.com.br>
2022-11-01 09:05:39 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json({ limit: "1000mb" }));
 
 cron.schedule("* * * * *", async (req, res) => {
-  console.log("Executando a tarefa a cada 1 minuto");
+  console.log("Executando a tarefa a cada 1 minuto ### INTERNAÇÃO PROD");
   const json = await getSantaJoana();
   console.res.json(json);
   return res.json(json);
@@ -18,8 +18,8 @@ app.get("/santa-joana", async (req, res) => {
   return res.json(json);
 });
 
-app.listen(8080, (err, data) => {
-  console.log("Ouvindo na porta 8080");
+app.listen(9292, (err, data) => {
+  console.log("Ouvindo na porta 9292");
 });
 
 export default app;
