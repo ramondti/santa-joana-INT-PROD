fa9e4e14751bd7c3d48251e190054f76189cf22d
Ramon Mattjie <97557540+ramondti@users.noreply.github.com>
2022-11-18 15:10:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.use(express.json({ limit: "1000mb" }));
 
-// cron.schedule("* * * * *", async (req, res) => {
-//   console.log("Executando a tarefa a cada 1 minuto ### INTERNAÇÃO PROD");
-//   const json = await getSantaJoana();
-//   console.res.json(json);
-//   return res.json(json);
-// });
+ cron.schedule("* * * * *", async (req, res) => {
+   console.log("Executando a tarefa a cada 1 minuto ### INTERNAÇÃO PROD");
+   const json = await getSantaJoana();
+   console.res.json(json);
+   return res.json(json);
+ });
 
 app.get("/santa-joana", async (req, res) => {
   const json = await getSantaJoana();
